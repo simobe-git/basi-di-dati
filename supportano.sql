2f9a5c216153bd032d80f3f3b9dba964cfa97201
simobe-git <166334938+simobe-git@users.noreply.github.com>
2024-10-07 16:35:54 +0200

    Add files via upload

---
+++ supportano.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Ott 07, 2024 alle 16:35
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `basi di dati`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `supportano`
+--
+
+CREATE TABLE `supportano` (
+  `codice_videogioco` int(5) NOT NULL,
+  `nome_hardware` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `supportano`
+--
+ALTER TABLE `supportano`
+  ADD KEY `codice_videogioco` (`codice_videogioco`),
+  ADD KEY `nome_hardware` (`nome_hardware`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
