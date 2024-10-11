983f5476040e86ee48a1158db7a2d433fe781078
Samuele Di Mario <samueledimario2@gmail.com>
2024-10-11 10:11:14 +0200

    modifica stile login

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php
 
-// Controllo se utente ha effettuato il login lo reindirizzo alla pagina di home  
-
+// Controllo se utente ha effettuato il login se si  lo reindirizzo alla pagina di home  
 session_start();
 if(isset($_SESSION['statoLogin'])){
     header('Location: home.php');
@@ -9,24 +8,22 @@ if(isset($_SESSION['statoLogin'])){
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Nome del Sito</title>
+    <title>Login</title>
     <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="home.css">
 </head>
 <body>
-
+    
     <nav class="navbar">
         <div class="logo">
             <a href="#">GameShop</a>
         </div>
         <ul class="nav-links">
-            <li><a href="home.php">Home</a></li>
+            <li><a href="#">Home</a></li>
             <li><a href="#">Giochi</a></li>
             <li><a href="#">Offerte</a></li>
             <li><a href="login.php">Login</a></li>
@@ -40,19 +37,26 @@ if(isset($_SESSION['statoLogin'])){
     </nav>
 
     <div class="login-container">
-        <h2>Accedi al tuo account</h2>
-        <form action="#" method="POST" class="login-form">
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit" class="cta-button">Accedi</button>
-            <p class="signup-link">Non hai un account? <a href="register.html">Registrati qui</a></p>
-        </form>
+        <div class="login-form">
+            <h2>SIGN <span class="highlight">IN</span></h2>
+            <form action="#" method="POST">
+                <div class="form-group">
+                    <input type="text" id="username" name="username" placeholder="Username or Email" required>
+                </div>
+                <div class="form-group">
+                    <input type="password" id="password" name="password" placeholder="Password" required>
+                </div>
+                <div class="form-group check-forgot">
+                    <label><input type="checkbox"> Stay signed in</label>
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <button type="submit" class="cta-button">SIGN IN</button>
+                <p class="signup-link">Not a member? <a href="#">Sign Up</a></p>
+            </form>
+        </div>
+        <div class="login-image">
+            <img src="../isset/background-login.jpg" alt="background">
+        </div>
     </div>
 </body>
 </html>
